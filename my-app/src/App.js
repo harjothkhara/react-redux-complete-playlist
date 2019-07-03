34911d002261f7ee7b88e1a4f547efe5724d361c
harjoth <harjoth.khara@gmail.com>
2019-07-03 11:42:48 -0700

    cycling through data using .map, we get the original array (ninjas), we map through that array, receive each individual ninja(represents each individual item in that array), then we perform a function for each individual ninja, inside that function we return a bit of jsx(which we want to output for each individual ninja), so it cycles through those , stores that jsx in a new array called ninjaList, then finally we output that ninjaList with all that jsx for us. That's how we loop through data and output it in React.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,13 +3,19 @@ import Ninjas  from './Ninjas.js';
 
 
 class App extends Component {
+    state = {
+      ninjas: [
+        { name: 'Ryu', age: 30, belt: 'black', id: 1 },
+        { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
+        { name: 'Crystal', age: 25, belt: 'pink', id: 3 },
+      ]
+    }
   render() {
     return (
       <div className="App">
         <h1>My first React App!</h1>
         <p>Welcome :)</p>
-        <Ninjas name="Ryu" age="25" belt="black" />
-        <Ninjas name="Yoshi" age="30" belt="black" />
+        <Ninjas ninjas={this.state.ninjas} />
       </div>
     );
   }
