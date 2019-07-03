9a6c798d7cff16f42c426b831d0226b7425546ac
harjoth <harjoth.khara@gmail.com>
2019-07-02 20:57:07 -0700

    created Ninjas component and nested inside App.js(root component). Keeping our code modular (Ninjas component may have its own state/methods) and reusable.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Ninjas  from './Ninjas.js';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My first React App!</h1>
-      <p>Welcome :)</p>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>My first React App!</h1>
+        <p>Welcome :)</p>
+        <Ninjas />
+      </div>
+    );
+  }
 }
 
 export default App;
