89458454e8e1386d6575f2579c905c4698e632a7
harjoth <harjoth.khara@gmail.com>
2019-07-05 20:04:51 -0700

    prefer to seperate the logic from the actual template itself. this is how we conditionally output something to the template. NO CHANGE. SAME RESULT

--- my-app/src/Ninjas.js
+++ my-app/src/Ninjas.js
@@ -2,24 +2,7 @@ import React from 'react';
 
 const Ninjas = ({ninjas}) => {
     // console.log(this.props);
-    // const ninjaList = ninjas.map(ninja => {
-    //   if (ninja.age > 20) {
-    //   return (
-    //      <div className="ninja" key={ninja.id}>
-    //         <div>Name: { ninja.name }</div>
-    //         <div>Age: { ninja.age }</div>
-    //         <div>Belt: { ninja.belt }</div>
-    //      </div>
-    //   )
-    //  } else {
-    //    return null
-    //  }
-    // })
-
-    return(
-      <div className="ninja-list">
-        {
-          ninjas.map(ninja => {
+    const ninjaList = ninjas.map(ninja => {
             return ninja.age > 20 ? (
               <div className="ninja" key={ninja.id}>
                   <div>Name: { ninja.name }</div>
@@ -28,7 +11,10 @@ const Ninjas = ({ninjas}) => {
                 </div>
             ) : null
           })
-        }
+
+    return(
+      <div className="ninja-list">
+        { ninjaList }
       </div>
     )
 }
